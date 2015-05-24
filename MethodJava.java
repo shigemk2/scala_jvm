fc6afaca983991c53acc6f7d5e62b248dc7761fa
shigemk2 <i.am.shige@gmail.com>
2015-05-24 19:14:02 +0900

    メソッド呼び出し 逆アセンブル

---
+++ MethodJava.java
@@ -0,0 +1,10 @@
+public class MethodJava {
+    public static int method(int a) {
+        return a;
+    }
+
+    public static void main(String[] args){
+        int a = 1;
+        System.out.println(method(a));
+    }
+}
---
+++ MethodScala.scala
@@ -0,0 +1,10 @@
+object MethodScala {
+  def method(a: Int): Int = {
+    a
+  }
+
+  def main(args: Array[String]) :Unit = {
+    val a = 1
+    println(method(a))
+  }
+}
