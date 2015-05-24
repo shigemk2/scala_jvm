9156dad6c2d9c76a12cb4cfe24e886555e440380
shigemk2 <i.am.shige@gmail.com>
2015-05-24 18:36:46 +0900

    Scala トレイト 逆アセンブル

---
+++ TraitScala.scala
@@ -0,0 +1,16 @@
+trait Trait {
+  def method(a:String, b:String): String
+}
+
+class TraitScala extends Trait {
+  def method(a:String, b:String): String = {
+    a + b
+  }
+}
+
+object TraitScala {
+  def main(args: Array[String]) :Unit = {
+    val a = new TraitScala()
+    println(a.method("a", "b"))
+  }
+}
