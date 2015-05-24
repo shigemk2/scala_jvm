9d0be7798bc840b55719ccd2874cc243076ae896
shigemk2 <i.am.shige@gmail.com>
2015-05-24 16:28:53 +0900

    階乗 逆アセンブル

---
+++ FactorialJava.java
@@ -0,0 +1,13 @@
+public class FactorialJava {
+    public static int fact(int n) {
+        if (n <= 0) {
+            return 1;
+        } else {
+            return n * fact(n - 1);
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(fact(5));
+    }
+}
---
+++ FactorialScala.scala
@@ -0,0 +1,10 @@
+object FactorialScala {
+  def fact(n: Int): Int = n match {
+    case 0 => 1
+    case n if n > 0 => n * fact(n - 1)
+  }
+
+  def main(args: Array[String]) :Unit = {
+    println(fact(5))
+  }
+}
