a5a341ae12e7e6212ec2a20a6d796f07e27298aa
shigemk2 <i.am.shige@gmail.com>
2015-05-24 14:09:11 +0900

    加算プログラム コンパイル 逆アセンブル

---
+++ PlusJava.java
@@ -0,0 +1,7 @@
+public class PlusJava {
+    public static void main(String[] args){
+        int a = 1;
+        a = a + 2;
+        System.out.println(a);
+    }
+}
---
+++ PlusScala.scala
@@ -0,0 +1,7 @@
+object PlusScala {
+  def main(args: Array[String]) :Unit = {
+    var a = 1
+    a = a + 2
+    println(a)
+  }
+}
