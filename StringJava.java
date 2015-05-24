bfb1a45224853f4f78ee9d30d7074027f96e3e32
shigemk2 <i.am.shige@gmail.com>
2015-05-24 16:07:21 +0900

    文字列連結 逆アセンブル

---
+++ StringJava.java
@@ -0,0 +1,7 @@
+public class StringJava {
+    public static void main(String[] args){
+        String a = "a" + "b";
+        a = "a" + "b";
+        System.out.println(a);
+    }
+}
---
+++ StringScala.scala
@@ -0,0 +1,7 @@
+object StringScala {
+  def main(args: Array[String]) :Unit = {
+    var a = "a" + "b"
+    a = "a" + "b"
+    println(a)
+  }
+}
