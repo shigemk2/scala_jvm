1c82741e9adc4c6bf62ed9dc24afc161f582d6fb
shigemk2 <i.am.shige@gmail.com>
2015-05-24 13:25:22 +0900

    HelloWorld.scalaをJD-GUIで逆コンパイルした

---
+++ HelloWorldDe$.java
@@ -0,0 +1,21 @@
+import scala.Predef.;
+
+public final class HelloWorld$
+{
+  public static final  MODULE$;
+  
+  static
+  {
+    new ();
+  }
+  
+  public void main(String[] args)
+  {
+    Predef..MODULE$.println("hello, world");
+  }
+  
+  private HelloWorld$()
+  {
+    MODULE$ = this;
+  }
+}
