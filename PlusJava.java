1e1baed0a466d7b57586c7ed113f3f7440013ffd
shigemk2 <i.am.shige@gmail.com>
2015-05-26 07:37:43 +0900

    加算 逆アセンブル(デバッグオプション付与)

--- PlusJava.java
+++ PlusJava.java
@@ -2,6 +2,5 @@ public class PlusJava {
     public static void main(String[] args){
         int a = 1;
         a = a + 2;
-        System.out.println(a);
     }
 }
--- PlusScala.scala
+++ PlusScala.scala
@@ -2,6 +2,5 @@ object PlusScala {
   def main(args: Array[String]) :Unit = {
     var a = 1
     a = a + 2
-    println(a)
   }
 }
