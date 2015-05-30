a909ff0e53b6d4923e3037efa5aab593a155cf81
shigemk2 <i.am.shige@gmail.com>
2015-05-30 18:30:50 +0900

    インターフェイスとトレイト

--- TraitScala.scala
+++ TraitScala.scala
@@ -1,16 +1,13 @@
 trait Trait {
   def method(a:String, b:String): String
 }
-
 class TraitScala extends Trait {
   def method(a:String, b:String): String = {
     a + b
   }
 }
-
 object TraitScala {
   def main(args: Array[String]) :Unit = {
     val a = new TraitScala()
-    println(a.method("a", "b"))
   }
 }
