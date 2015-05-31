9caf4b0975c0a555ecc66c41ae8c705420be0467
shigemk2 <i.am.shige@gmail.com>
2015-05-31 21:12:18 +0900

    Add javap pattern

---
+++ PlusJavaBeta.java
@@ -0,0 +1,10 @@
+class PlusJava {
+    private static int method(int x) {
+        return x;
+    }
+    public static void main(String[] args){
+        int a = 1;
+        a = a + 2;
+        method(a);
+    }
+}
