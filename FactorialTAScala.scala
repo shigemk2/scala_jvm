1d3777fe960db7ffb48b0393aa63a36930f43076
shigemk2 <i.am.shige@gmail.com>
2015-06-04 23:23:08 +0900

    末尾再帰

--- FactorialTAScala.scala
+++ FactorialTAScala.scala
@@ -1,5 +1,8 @@
+import scala.annotation.tailrec
+
 object FactorialTAScala {
   def fact(n: Int): Int = {
+    @tailrec
     def go(n: Int, acc: Int): Int =
       if (n <= 0) acc
       else go(n - 1, n * acc)
